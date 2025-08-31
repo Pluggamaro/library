3b065e61a7ed341cc24444aaa1a83f18ed8b16a3
Bongani X <velanibzuma@gmail.com>
2025-09-01 00:41:37 +0200

    -implemented a book class(model) with getters and setters and an overriden toString method for custom data representation

---
+++ src/main/java/com/library/Book.java
@@ -0,0 +1,28 @@
+package com.library;
+
+public class Book {
+    private int id;
+    private String title;
+    private boolean isAvailable;
+    private String author;
+
+    public Book(int id, String title, String author, boolean isAvailable){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId(){return id;}
+    public String getAuthor(){return author;}
+    public boolean isAvailable(){ return isAvailable;}
+    public String getTitle(){return title;}
+
+    //overrideing the toString method from string class from custom representations
+
+    @override
+    public String toString(){
+        return String.format("ID: %-3d | Title: %-30s | Author: %-20s | Available: %s",
+                                    id,title,author,(isAvailable?"Yes":"No") );
+    }
+}
