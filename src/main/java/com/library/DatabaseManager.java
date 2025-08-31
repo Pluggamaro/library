6c78eb9be24ead0608028e8bfb6b863220c5558b
Bongani X <velanibzuma@gmail.com>
2025-09-01 00:28:20 +0200

    -inserted hardcoded jdbc connection parameters -DatabaseManager class

---
+++ src/main/java/com/library/DatabaseManager.java
@@ -0,0 +1,20 @@
+package com.library;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    //hardcoding database connection parameters
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/library";
+    private static final String USER = "AdminOne";
+    private static final String PASSWORD = "El3l0h31$r@3l";
+    
+    //connection to database
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+
+}
